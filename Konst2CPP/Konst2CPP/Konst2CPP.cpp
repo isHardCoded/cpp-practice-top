35de4c4d665348e66090128bc4edeb6138d7e3a4
ishardcoded <ishardcoded@gmail.com>
2024-10-31 16:50:21 +0500

    Added constructor project

---
+++ Konst2CPP/Konst2CPP/Konst2CPP.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+using namespace std;
+
+class House {
+private:
+    int rooms;
+    double s;
+    string year;
+    string address;
+
+public:
+    
+    House(int valueRooms, double valueS, string valueYear, string valueAddress) {
+        rooms = valueRooms;
+        s = valueS;
+        year = valueYear;
+        address = valueAddress;
+    }
+
+    void DisplayInfo() {
+        cout << rooms << endl << s << endl << year << endl << address;
+    }
+
+    static House InputInfo() {
+        int rooms;
+        double s;
+        string year;
+        string address;
+
+        cout << "Введите кол-во комнат: " << endl;
+        cin >> rooms;
+
+        cin >> s;
+        cin >> year;
+        cin >> address;
+
+        return House(rooms, s, year, address);
+    }
+
+    ~House() {
+        cout << "Object was removed" << endl;
+    }
+};
+
+
+int main()
+{
+    
+    House house1 = House::InputInfo();
+    house1.DisplayInfo();
+
+}
