5cb1a530ca2d47f85c73891b4f89d47a0abec0d6
ishardcoded <ishardcoded@gmail.com>
2024-11-05 18:50:36 +0500

    Added templates projects

---
+++ TemplateCPP/TemplateCPP/TemplateCPP.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+// Шаблоны классов
+template <typename T1, typename T2, typename T3>
+
+class Pair {
+private:
+	T1 first;
+	T2 second;
+	T3 test;
+
+public:
+	Pair(T1 f, T2 s, T3 t) : first(f), second(s), test(t) {}
+
+	T1 getFirst() const {
+		return first;
+	}
+
+	T2 getSecond() const {
+		return second;
+	}
+
+	T3 getTest() const {
+		return test;
+	}
+};
+
+int main()
+{
+	// Создать разные пары значений с разными типами данных
+	// для разных объектов
+
+	Pair<std::string, double, int> intPair("Hello", 1.3, 3);
+	Pair<char, double, int> intPair(9, 1.3, 3);
+
+}
